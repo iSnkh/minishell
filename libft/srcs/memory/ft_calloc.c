b8c6a4103f657263c4e0d8ecd81db7842bf033cb
Alexis M <iamsnkh@pm.me>
2021-01-10 13:02:39 +0100

    :sparkles: add: Libft

---
+++ libft/srcs/memory/ft_calloc.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amonteli <amonteli@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/13 20:49:48 by amonteli          #+#    #+#             */
+/*   Updated: 2020/07/16 03:39:51 by amonteli         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	char	*tab;
+
+	if (!(tab = (char *)malloc(nmemb * size)))
+		return (NULL);
+	return (ft_memset(tab, 0, nmemb * size));
+}
